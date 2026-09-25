eb8d59ec7c3b7dfa76b6756879a2f5f29e7684c6
Michael Ma <michael.ma.ibm@outlook.com>
2026-09-25 10:57:16 -0400

    Remove reflection from provider instantiation in tests
    Multiple places use reflection to create providers unnecessarily.
    Replace `Class.forName(...).getDeclaredConstructor().newInstance()` with
    direct constructors for `OpenJCEPlus`, `OpenJCEPlusFIPS`, and
    `BouncyCastleProvider`.
    
    Fixes: https://github.com/IBM/OpenJCEPlus/issues/1709
    
    Signed-off-by: Michael Ma <michael.ma@ibm.com>

--- src/test/java/ibm/jceplus/jmh/JMHBase.java
+++ src/test/java/ibm/jceplus/jmh/JMHBase.java
@@ -8,15 +8,19 @@
 
 package ibm.jceplus.jmh;
 
+import com.ibm.crypto.plus.provider.OpenJCEPlus;
+import com.ibm.crypto.plus.provider.OpenJCEPlusFIPS;
 import java.io.File;
 import java.lang.reflect.Field;
 import java.net.URL;
 import java.net.URLClassLoader;
 import java.security.Provider;
+import java.security.Security;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
+import org.bouncycastle.jce.provider.BouncyCastleProvider;
 import org.openjdk.jmh.profile.ClassloaderProfiler;
 import org.openjdk.jmh.profile.CompilerProfiler;
 import org.openjdk.jmh.profile.GCProfiler;
@@ -163,20 +167,17 @@ abstract public class JMHBase {
 
     protected void insertProvider(String provider) throws Exception {
         if (provider.equalsIgnoreCase("OpenJCEPlus")) {
-            Provider myProvider = java.security.Security.getProvider("OpenJCEPlus");
+            Provider myProvider = Security.getProvider("OpenJCEPlus");
             if (myProvider == null) {
-                myProvider = (Provider) Class.forName("com.ibm.crypto.plus.provider.OpenJCEPlus")
-                        .getDeclaredConstructor().newInstance();
+                myProvider = new OpenJCEPlus();
             }
-            java.security.Security.insertProviderAt(myProvider, 1);
+            Security.insertProviderAt(myProvider, 1);
         } else if (provider.equalsIgnoreCase("BC")) {
-            Provider myProvider = java.security.Security.getProvider("BC");
+            Provider myProvider = Security.getProvider("BC");
             if (myProvider == null) {
-                myProvider = (Provider) Class
-                        .forName("org.bouncycastle.jce.provider.BouncyCastleProvider")
-                        .getDeclaredConstructor().newInstance();
+                myProvider = new BouncyCastleProvider();
             }
-            java.security.Security.insertProviderAt(myProvider, 1);
+            Security.insertProviderAt(myProvider, 1);
         }
     }
 }
